6de16b675ffbb5778682ded556c2e076833aac52
Faith1sGay <49874713+Faith1sGay@users.noreply.github.com>
2020-10-31 13:15:24 -0400

    Pog, i did it

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SomeProgram
+
+
+{
+    class Square
+    {
+        public int x, y;
+        public Square(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class SquareMultiply
+    {
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Square sq1 = new Square(3, 9);
+                Square sq2 = new Square(10, 5);
+                Console.WriteLine($"The Value of both squares together is {sq1.x * sq1.y + sq2.x + sq2.y}");
+                Console.ReadLine();
+
+
+            }
+
+        }
+    }
+}
